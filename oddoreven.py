9dd6e668d3191c7eb143d2fa68b33dea1d5e37d9
albin234 <gopalgenjimagar@gmail.com>
2017-12-10 21:49:15 +0545

    acceleration calculator

---
+++ oddoreven.py
@@ -0,0 +1,11 @@
+def checker():
+    input_num=int(input('enter any number:'))
+
+    if input_num%2==0:
+        print(input_num,"is EVEN")
+    else:
+        print(input_num,"is ODD")
+
+checker()
+
+0
